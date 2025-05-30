67ac99b92013849cfdc5a8a43d23036c446378ef
Tuan Anh <tuan73176@gmail.com>
2025-05-30 16:59:42 +0700

    casuarina_open-user 10 QKQ1.200311.002 V430A_OPN_U_B15_211112 release-keys
    Change-Id: Ib102b101a61c78e184a19feccebf0072f38a438d

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),casuarina)
+
+$(call add-radio-file-sha1-checked,radio/aboot.mbn,aab59f237734976a1f0b49a5d21760b099901f91)
+$(call add-radio-file-sha1-checked,radio/cmnlib.mbn,132817e060ca24e356f42e03f361ad3347de7ed3)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.mbn,f5c712fde3cb16bc9d8d88fa397c269dab3c48ee)
+$(call add-radio-file-sha1-checked,radio/devcfg.mbn,54b00e4612ff7a5fa74587d2c99fa9357a17d880)
+$(call add-radio-file-sha1-checked,radio/dsp.bin,67c7b54b49cb21e6d9b08d037c2b7c68b9ce65e7)
+$(call add-radio-file-sha1-checked,radio/keymaster.mbn,dad7a242dcbd727bad60c21012f237e9a3f6df78)
+$(call add-radio-file-sha1-checked,radio/lksecapp.mbn,8f1832d15a1deef559ad4e25725533711891ce9f)
+$(call add-radio-file-sha1-checked,radio/mdtp.img,41e15187c7cb4e8b756b2e08f811d0e1f40a41c6)
+$(call add-radio-file-sha1-checked,radio/modem.bin,e5acdfe55674cef37e8b5a20e17fb90182c25fbe)
+$(call add-radio-file-sha1-checked,radio/rpm.mbn,be1cf52faa67fdbd1071acad32608d9a65d5c364)
+$(call add-radio-file-sha1-checked,radio/sbl1.mbn,1435e28593935c899d3f9b046c51eb89b74b1846)
+$(call add-radio-file-sha1-checked,radio/splash.img,4ef3dfa541f0cdad23dc74a94bd73fe9a629f27d)
+$(call add-radio-file-sha1-checked,radio/tz.mbn,837cc86c2474699f214badb809a2c72205f7bdd7)
+
+endif
